5fa182a48f24b68f67f41a9d77a55deab0e39f0b
José Gabriel Paludo <118085261+jose-gp21@users.noreply.github.com>
2023-05-24 09:30:01 -0300

    Update BottomTab.js

--- src/navigations/BottomTab.js
+++ src/navigations/BottomTab.js
@@ -1,169 +1,243 @@
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation, DrawerActions } from "@react-navigation/native";
 
-import { View, TouchableOpacity} from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import {Ionicons} from '@expo/vector-icons'
-import { useNavigation, DrawerActions } from '@react-navigation/native';
-
-
-import Inicio from '../screens/home/inicio';
-import Report from '../screens/home/cadastrarproblema';
-import Info from '../screens/home/info'
-import Perfil from '../screens/home/perfil';
-import Mapainteiro from '../screens/home/mapa/mapainteiro';
+import Inicio from "../screens/home/inicio";
+import Report from "../screens/home/cadastrarproblema";
+import Info from "../screens/home/info";
+import Perfil from "../screens/home/perfil";
+import Mapainteiro from "../screens/home/mapa/mapainteiro";
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
-
   const navigation = useNavigation();
 
   return (
-
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarActiveTintColor: 'white',
-          tabBarInactiveTintColor: '#cccccc',
-          tabBarShowLabel: false,
-          tabBarStyle: {
-            backgroundColor: '#2FAD8F',
-            padding: 2,
-            height: 50,
-            borderRadius: 10,
-            bottom: 10,
-            marginHorizontal: 10,
-            shadowOffset: {
-              width: 10,
-              height: 10,
-            },
-            shadowColor: 'black',
-            shadowOpacity: 0.5,
-            elevation: 20,
-            
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "#cccccc",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#2FAD8F",
+          padding: 2,
+          height: 50,
+          borderRadius: 10,
+          bottom: 10,
+          marginHorizontal: 10,
+          shadowOffset: {
+            width: 10,
+            height: 10,
           },
-        })}
-      >
-        <Tab.Screen
-          name="Informações"
-          component={Info}
-          options={{
-            headerStyle:
-            {
-              backgroundColor: '#2FAD8F',
-              height: 100,
-              borderRadius: 15
-            },
-            headerTitleStyle: {
-              color: 'white',
-              
-            },
-            headerShown: true,
-            tabBarIcon: ({ color, size, focused }) => {
-              if (focused) {
-                return <Ionicons name='bookmark' size={22} color={color} />
-              }
-              return <Ionicons name='bookmark-outline' size={22} color={color} />
-            },
-            headerRight: () => {
-              return (
-                <TouchableOpacity onPress={() => navigation.openDrawer()}>
-                  <Ionicons name='menu' size={30} color='white' />              
-                </TouchableOpacity>
-              );
-            },
-          }}
-        />
-        <Tab.Screen name="Início" component={Inicio} 
+          shadowColor: "black",
+          shadowOpacity: 1,
+          elevation: 20,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Informações"
+        component={Info}
         options={{
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#2FAD8F",
+            height: 100,
+            borderRadius: 15,
+          },
+          headerTitleStyle: {
+            color: "white",
+          },
           headerShown: true,
-          headerStyle:
-            {
-              backgroundColor: '#2FAD8F',
-              height: 100,
-              borderRadius: 15
-            },
-            headerTitleStyle: {
-              color: 'white',
-              
-            },
           tabBarIcon: ({ color, size, focused }) => {
             if (focused) {
-              return <Ionicons name='home' size={22} color={color} />
+              return <Ionicons name="bookmark" size={22} color={color} />;
             }
-            return <Ionicons name='home-outline' size={22} color={color} />
+            return <Ionicons name="bookmark-outline" size={22} color={color} />;
           },
-          headerRight: () => {
+          headerLeft: () => {
             return (
-              <TouchableOpacity onPress={()=>{navigation.openDrawer()}}>
-                 <Ionicons name='menu' size={30} color='white' />              
+              <TouchableOpacity style={styles.container2}>
+                <Ionicons name="person-outline" size={22} color="black" />
               </TouchableOpacity>
             );
           },
-        }}/>
-        
-        <Tab.Screen name="Mapa" component={Mapainteiro} 
+          headerRight: () => {
+            return (
+              <TouchableOpacity
+                style={styles.container}
+                onPress={() => navigation.openDrawer()}
+              >
+                <Ionicons name="menu" size={30} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Início"
+        component={Inicio}
         options={{
           headerShown: true,
-          headerStyle:
-            {
-              backgroundColor: '#2FAD8F',
-              height: 100,
-              borderRadius: 15
-            },
-            headerTitleStyle: {
-              color: 'white',
-              
-            },
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#2FAD8F",
+            height: 100,
+            borderRadius: 15,
+          },
+          headerTitleStyle: {
+            color: "white",
+          },
           tabBarIcon: ({ color, size, focused }) => {
             if (focused) {
-              return <Ionicons name='map' size={22} color={color} />
+              return <Ionicons name="home" size={22} color={color} />;
             }
-            return <Ionicons name='map-outline' size={22} color={color} />
-          }
-        }}/>
+            return <Ionicons name="home-outline" size={22} color={color} />;
+          },
+          headerRight: () => {
+            return (
+              <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+              >
+                <Ionicons name="menu" size={30} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
 
-        <Tab.Screen name="Reportar" component={Report} 
+      <Tab.Screen
+        name="Mapa"
+        component={Mapainteiro}
+        options={{
+          headerTitleAlign: "center",
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#2FAD8F",
+            height: 100,
+            borderRadius: 15,
+          },
+          headerTitleStyle: {
+            color: "white",
+          },
+          tabBarIcon: ({ color, size, focused }) => {
+            if (focused) {
+              return <Ionicons name="map" size={22} color={color} />;
+            }
+            return <Ionicons name="map-outline" size={22} color={color} />;
+          },
+          headerRight: () => {
+            return (
+              <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+              >
+                <Ionicons name="menu" size={30} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="Reportar"
+        component={Report}
         options={{
           headerShown: true,
-          headerStyle:
-            {
-              backgroundColor: '#2FAD8F',
-              height: 100,
-              borderRadius: 15
-            },
-            headerTitleStyle: {
-              color: 'white',
-              
-            },
+          headerStyle: {
+            backgroundColor: "#2FAD8F",
+            height: 100,
+            borderRadius: 15,
+          },
+          headerTitleStyle: {
+            color: "white",
+          },
+          headerTitleAlign: "center",
           tabBarIcon: ({ color, size, focused }) => {
             if (focused) {
-              return <Ionicons name='add' size={40} color={color} />
+              return <Ionicons name="add" size={40} color={color} />;
             }
-            return <Ionicons name='add-outline' size={22} color={color} />
-          }
-        }}/>
+            return <Ionicons name="add-outline" size={22} color={color} />;
+          },
+          headerRight: () => {
+            return (
+              <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+              >
+                <Ionicons name="menu" size={30} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
 
-        <Tab.Screen name="Perfil" component={Perfil} 
+      <Tab.Screen
+        name="Perfil"
+        component={Perfil}
         options={{
           headerShown: true,
-          headerStyle:
-            {
-              backgroundColor: '#2FAD8F',
-              height: 100,
-              borderRadius: 15
-            },
-            headerTitleStyle: {
-              color: 'white',
-              
-            },
+          headerTitleAlign: "center",
+          headerStyle: {
+            backgroundColor: "#2FAD8F",
+            height: 60,
+            borderRadius: 15,
+          },
+          headerTitleStyle: {
+            color: "white",
+          },
           tabBarIcon: ({ color, size, focused }) => {
             if (focused) {
-              return <Ionicons name='person' size={22} color={color} />
+              return <Ionicons name="person" size={22} color={color} />;
             }
-            return <Ionicons name='person-outline' size={22} color={color} />
-          }
-        }}/>
-
-      </Tab.Navigator>
+            return <Ionicons name="person-outline" size={22} color={color} />;
+          },
+          headerRight: () => {
+            return (
+              <TouchableOpacity
+                style={styles.container}
+                onPress={() => {
+                  navigation.openDrawer();
+                }}
+              >
+                <Ionicons name="menu" size={30} color="black" />
+              </TouchableOpacity>
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    right: 10,
+    height: 35,
+    width: 35,
+    marginVertical: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20
+  },
+  container2:{
+    left: 10,
+    height: 35,
+    width: 35,
+    marginVertical: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20
+  }
+});
